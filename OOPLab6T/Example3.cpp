074e4185fcc279b53c52e05ee54490ee453fa9df
YuraStaiL <amator111555@gmail.com>
2023-02-27 09:55:37 +0200

    comleted 2 and 3 tasks

--- OOPLab6T/Example3.cpp
+++ OOPLab6T/Example3.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Lab6Example.h"
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 namespace SpaceExample3 {
     // MultipleiInherance.cpp 
@@ -9,85 +10,88 @@ namespace SpaceExample3 {
 //
 
 
-    class Car {
+    class File {
     protected:
-        string marka;
-        float power;
-        int numberOfWheels;
+        std::string filename;
     public:
-        Car() : marka("Neoplan"), power(5.2f), numberOfWheels(6) {
-        }
-        Car(string m, float p, int nw) : marka(m), power(p), numberOfWheels(nw) {
-        }
-        string getMarka() { return marka; }
-        void setMarka(string m) { marka = m; }
-        float getPower() {
-            return power;
-        }
-        void setPower(float p) {
-            power = p;
-        }
-        int getNumberOfWheels() {
-            return numberOfWheels;
-        }
-        void setNumberOfWheels(int n) {
-            numberOfWheels = n;
-        }
-        string toString() {
-            string r = marka + "\t" + to_string(power) + "\t" + to_string(numberOfWheels) + "\t";
-            return r;
+        File(const std::string& filename) : filename(filename) {}
+        virtual ~File() {}
+        virtual void read() const = 0;
+        virtual void write() const = 0;
+        friend std::ostream& operator<<(std::ostream& os, const File& file);
+    };
+
+    std::ostream& operator<<(std::ostream& os, const File& file) {
+        os << "Filename: " << file.filename << std::endl;
+        return os;
+    }
+
+    class Readable {
+    public:
+        virtual void read() const = 0;
+    };
+
+    class Writable {
+    public:
+        virtual void write() const = 0;
+    };
+
+    class ReadWrite : public Readable, public Writable {
+    public:
+        virtual void read() const = 0;
+        virtual void write() const = 0;
+    };
+
+    class ReadableFile : public File, public Readable {
+    public:
+        ReadableFile(const std::string& filename) : File(filename) {}
+        virtual void read() const {
+            std::ifstream file(filename);
+            std::cout << "Content of " << filename << ":" << std::endl;
+            std::string line;
+            while (std::getline(file, line)) {
+                std::cout << line << std::endl;
+            }
         }
     };
 
-    class PassengerTransport {
-    protected:
-        int flightNumber;
-        int numberOfPassengerSeats;
+    class WritableFile : public File, public Writable {
     public:
-        PassengerTransport() : flightNumber(101), numberOfPassengerSeats(45) {}
-        PassengerTransport(int f, int n) : flightNumber(f), numberOfPassengerSeats(n) {}
-        int  getFlightNumber() { return flightNumber; }
-        void setFlightNumber(int f) { flightNumber = f; }
-        int getNumberOfPassengerSeats() { return numberOfPassengerSeats; }
-        void setnumberOfPassengerSeats(int n) { numberOfPassengerSeats = n; }
-        string toString() {
-            string r = to_string(flightNumber) + "\t" + to_string(numberOfPassengerSeats) + "\t";
-            return r;
+        WritableFile(const std::string& filename) : File(filename) {}
+        virtual void write() const {
+            std::ofstream file(filename);
+            file << "Hello, world!" << std::endl;
         }
     };
-    class AutoBus : public Car, public  PassengerTransport
-    {
-        string busRoute;
-    public:
-        AutoBus() : busRoute("Kyiv-Chernivci") {}
-        AutoBus(string m, float p, int nw, int f, int n, string bs)
-            : Car(m, p, nw), PassengerTransport(f, n), busRoute(bs) {}
-        string getbusRoute() {
-            return busRoute;
-        }
-        void setbusRoute(string bs) { busRoute = bs; }
 
-        string toString() {
-            string r = Car::toString() + PassengerTransport::toString() + busRoute;
-            return r;
+    class ReadWriteFile : public File, public ReadWrite {
+    public:
+        ReadWriteFile(const std::string& filename) : File(filename) {}
+        virtual void read() const {
+            std::ifstream file(filename);
+            std::cout << "Content of " << filename << ":" << std::endl;
+            std::string line;
+            while (std::getline(file, line)) {
+                std::cout << line << std::endl;
+            }
+        }
+        virtual void write() const {
+            std::ofstream file(filename);
+            file << "Some text" << std::endl;
         }
     };
+
+
 
     int mainExample3()
     {
-        AutoBus def;
-        AutoBus lvCh("Iveko", 6.2f, 6, 301, 40, "Lviv-Chernivci");
-        AutoBus* pVnCn = new AutoBus();
-        pVnCn->setMarka("Ikarus");
-        pVnCn->setPower(7.2f);
-        pVnCn->setNumberOfWheels(6);
-        pVnCn->setFlightNumber(403);
-        pVnCn->setnumberOfPassengerSeats(42);
-        pVnCn->setbusRoute("Vinnicya-Chernivci");
+        File* f3 = new ReadWriteFile("data.txt");
+        std::cout << *f3;
+        f3->read();
+        f3->write();
+        delete f3;
 
-        cout << def.toString() << endl;
-        cout << lvCh.toString() << endl;
-        cout << pVnCn->toString() << endl;
+
         return 0;
     }
 
